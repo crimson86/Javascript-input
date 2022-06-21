624a028ff50ff5ddd72333c2f7773dd29cbe3df3
crimson86 <nderitudaniel8@gmail.com>
2022-06-21 10:49:34 +0300

    input

---
+++ input.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+var fname=prompt("Enter Your Name:");
+var height=prompt("Enter Your Height:");
+var count=prompt("Enter Your Country:");
+console.log("Your name is",fname);
+console.log("Your Height is",height);
+console.log("Your Country is",count);
